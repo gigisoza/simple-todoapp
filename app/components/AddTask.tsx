4e03e3fe6cc4c08db15f20c694b78267001f7d3b
gigisoza <sozashviligigi@gmail.com>
2023-08-18 23:34:00 +0400

    Create form in AddTask.tsx to add todo items in project

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -6,6 +6,13 @@ import Modal from "./Modal";
 
 export default function AddTask() {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+
+  const handleSubmitNewTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(newTaskValue);
+    setNewTaskValue("");
+  };
 
   return (
     <div>
@@ -16,7 +23,21 @@ export default function AddTask() {
         Add new task <AiOutlinePlus className="ml-2" size={18} />
       </button>
       <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
-        modal for add todo
+        <form onSubmit={handleSubmitNewTodo}>
+          <h3 className="font-bold text-lg">Add new task</h3>
+          <div className="modal-action">
+            <input
+              value={newTaskValue}
+              onChange={(e) => setNewTaskValue(e.target.value)}
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-full"
+            />
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
