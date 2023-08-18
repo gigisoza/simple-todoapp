a79dbb8b5b8b98f9f0a687095836d8d58114f5d0
gigisoza <sozashviligigi@gmail.com>
2023-08-19 00:58:05 +0400

    Add edit functionality to edit todo items

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,15 +1,63 @@
+"use client";
+
 import { ITask } from "@/types/tasks";
-import React from "react";
+import { FiEdit } from "react-icons/fi";
+import { FaTrashAlt } from "react-icons/fa";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Modal from "./Modal";
+import { editTodo } from "@/api";
 
 interface Props {
   task: ITask;
 }
 
 export default function Task({ task }: Props) {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setTaskToEdit("");
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
   return (
     <tr key={task.id}>
-      <td>{task.text}</td>
-      <td>Blue</td>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-5">
+        <FiEdit
+          onClick={() => setOpenModalEdit(true)}
+          cursor="pointer"
+          className="text-blue-500"
+          size={25}
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Edit Task</h3>
+            <div className="modal-action">
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FaTrashAlt cursor="pointer" className="text-red-500" size={25} />
+      </td>
     </tr>
   );
 }
